d0965c3a08008f44deaee2c41274a6fc5d7ca7c0
Ella Mathewson <ellaofohio@gmail.com>
2019-11-10 21:36:44 -0500

    Changed name from domo to data

--- server/controllers/FoodData.js
+++ server/controllers/FoodData.js
@@ -3,10 +3,10 @@
 /* eslint-disable no-console */
 const models = require('../models');
 
-const Domo = models.Domo;
+const Data = models.Data;
 
 const makerPage = (req, res) => {
-  Domo.DataModel.findByOwner(req.session.account._id, (err, docs) => {
+  Data.DataModel.findByOwner(req.session.account._id, (err, docs) => {
     if (err) {
       console.log(err);
       return res.status(400).json({ error: 'An error occured' });
@@ -15,7 +15,7 @@ const makerPage = (req, res) => {
   });
 };
 
-const makeDomo = (req, res) => {
+const makePost = (req, res) => {
   if (!req.body.name || !req.body.age) {
     return res.status(400).json({ error: 'RAWR! Both name and age are required ' });
   }
@@ -27,13 +27,13 @@ const makeDomo = (req, res) => {
     owner: req.session.account._id,
   };
 
-  const newDomo = new Domo.DataModel(domoData);
+  const newDomo = new Data.DataModel(domoData);
 
-  const domoPromise = newDomo.save();
+  const dataPromise = newDomo.save();
 
-  domoPromise.then(() => res.json({ redirect: '/maker' }));
+  dataPromise.then(() => res.json({ redirect: '/maker' }));
 
-  domoPromise.catch((err) => {
+  dataPromise.catch((err) => {
     console.log(err);
     if (err.code === 11000) {
       return res.status(400).json({ error: 'Domo already exists' });
@@ -42,8 +42,8 @@ const makeDomo = (req, res) => {
     return res.status(400).json({ error: 'An error occured' });
   });
 
-  return domoPromise;
+  return dataPromise;
 };
 
 module.exports.makerPage = makerPage;
-module.exports.make = makeDomo;
+module.exports.make = makePost;
